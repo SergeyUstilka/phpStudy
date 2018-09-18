354c0a4988f78346660f8b29d1aba032e93f7004
Sergey Ustsilka <ya.sg-one@yandex.ru>
2018-09-18 12:34:31 +0300

    Задача с циферблатом

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<form action="result.php" method="post">
+    <input type="text" name="chislo">
+    <input type="submit" value="Отправить">
+</form>
 <?php
 /**
  * Created by PhpStorm.
@@ -7,85 +11,9 @@
  */
 
 
-$a = 2;
-$b = -4;
-$c = -3;
-echo "Даны числа:<b> $a  $b $c  </b></br>";
-// даны три числа - вывести кол-во положительных и отрицательных
-$otr = 0;
-$pol = 0;
-if ($a > 0 and $a!= 0) {
-    $pol++;
-} else {
-    $otr++;
-}
-if ($b > 0 and $b != 0 ) {
-    $pol++;
-} else {
-    $otr++;
-}
-if ($c > 0 and $c != 0) {
-    $pol++;
-} else {
-    $otr++;
-}
-echo "Количество положительных: $pol  </br> Количество отрицательных: $otr  </br>";
 
-//даны три числа - вывести кол-во четных и нечетных
-$chet = 0;
-$nechet = 0;
-if ($a % 2 == 0) {
-    $chet++;
-} else {
-    $nechet++;
-}
-if ($b % 2 == 0) {
-    $chet++;
-} else {
-    $nechet++;
-}
-if ($c % 2 == 0) {
-    $chet++;
-} else {
-    $nechet++;
-}
-echo "Количество четных: $chet </br> Количество нечетных: $nechet  </br>";
-//даны три числа - вывести кол-во четных и нечетных
-$chet = 0;
-$nechet = 0;
-if ($a % 2 == 0) {
-    $chet++;
-} else {
-    $nechet++;
-}
-if ($b % 2 == 0) {
-    $chet++;
-} else {
-    $nechet++;
-}
-if ($c % 2 == 0) {
-    $chet++;
-} else {
-    $nechet++;
-}
-echo "Количество четных: $chet </br> Количество нечетных: $nechet  </br>";
+echo "<pre>";
+
+echo "</pre>";
 
 
-//даны три числа - вывести среднее
-$mas = [2,-4,-3];
-$min = $mas[0];
-$max = $mas[0];
-for ($i=1;$i<3; $i++){
-    if($mas[$i] > $max){
-        $max = $mas[$i];
-    }
-    elseif($mas[$i] < $min){
-        $min = $mas[$i];
-    }
-}
-for($i=0;$i<3; $i++){
-    if ($mas[$i] != $max  and $mas[$i] != $min){
-        echo "Среднее число $mas[$i]";
-        break;
-    }
-}
