9fa62d78637aa95fdef411fca17006c738879242
Sergey Ustsilka <ya.sg-one@yandex.ru>
2018-10-12 11:32:44 +0300

    1

--- index.php
+++ index.php
@@ -3,13 +3,7 @@
 include_once (__DIR__."/config/config.php");
 // Proveriam ne glavnaia li et stranicia
 
-if ($_SERVER['REQUEST_URI'] == '/' || !empty($_GET['pag'])){
-    $pagetype ='main';
-}elseif(parse_url($_SERVER['REQUEST_URI'])['path'] == '/single'){
-    $pagetype = 'single';
-}else{
-    $pagetype= $_SERVER['REQUEST_URI'];
-}
+include_once ('include/router.php');
 
 include_once (__DIR__."/include/functions.php");
 include_once (__DIR__."/template/layout.php");
