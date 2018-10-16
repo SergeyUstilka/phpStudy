156a156e1bfd40d08f276fa0d7c196d3e64b8fe2
Sergey Ustsilka <ya.sg-one@yandex.ru>
2018-10-16 21:53:07 +0300

    Navel nemnogo losku1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once (__DIR__."/config/config.php");
-
+print_r($_SERVER['REQUEST_URI']);
 if ($_SERVER['REQUEST_URI'] == '/' || !empty($_GET['pag'])){
     $pagetype ='main';
 }elseif(parse_url($_SERVER['REQUEST_URI'])['path'] == '/single'){
